02d1bbcc92b891621b33a3ae7747109c02a8ee53
Adebayo-jzs <adedejiadebayo732@gmail.com>
2025-11-17 23:44:08 +0100

    icon

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
