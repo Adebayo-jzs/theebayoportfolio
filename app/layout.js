565b66186e0ba27918eb54c10235c06c60a2e876
Adebayo <adedejiadebayo732@gmail.com>
2026-01-17 18:36:13 +0100

    added scrollprogress

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
+"use client"
 import { Analytics } from "@vercel/analytics/next";
 import { Geist, Geist_Mono } from "next/font/google"; 
 import CursorWrapper from "@/components/CursorWrapper";
+import React,{useState,useEffect} from "react";
+import { North } from "@mui/icons-material";
 
 import "./globals.css";
 // import Navbar from "@/components/Navbar";
@@ -20,6 +23,19 @@ const geistMono = Geist_Mono({
 // };
 
 export default function RootLayout({ children }) {
+  const [scrollProgress,setScrollProgress] = useState(0);
+  // const scrollRef = useRef(null);
+
+  useEffect(() => {
+		const handleScroll = () => {
+			const totalScroll = document.documentElement.scrollTop;
+			const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+			const scroll = `${totalScroll / windowHeight}`;
+			setScrollProgress(Number(scroll)); 
+		}
+		window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <html lang="en">
       <head>
@@ -80,6 +96,27 @@ export default function RootLayout({ children }) {
           {children}
         </CursorWrapper>
         <Analytics/>
+
+        <div className="block fixed bottom-8 right-3 mix-blend-difference z-50">
+        <div className="flex flex-col items-center gap-6">
+          <span className="[writing-mode:vertical-rl] text-[11px] font-black tracking-[0.6em] text-white uppercase">SCROLL TO EXPLORE</span>
+          <div className="h-16 w-[2px] bg-white/20 relative overflow-hidden">
+            <div 
+              className="absolute top-0 left-0 w-full bg-white transition-all duration-100 ease-out" 
+              style={{ height: `${scrollProgress * 100}%` }}
+            ></div>
+          </div>
+        </div>
+        <button
+          onClick={() =>
+            window.scrollTo({ top: 0, behavior: "smooth" })
+          }
+          className="border rounded-full p-2 mt-3"
+        >
+          <North/>
+        </button>
+
+      </div>
       </body>
     </html>
   );
