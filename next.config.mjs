8708a4422a7bc9ccf32e0a238a242c9ccfc65ad0
Adebayo <adedejiadebayo732@gmail.com>
2026-01-26 23:32:33 +0100

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
+import withOutray from '@outray/next';
+// const nextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+export default withOutray(
+  {
+
+  },
+  {
+    subdomain: 'textcognito',
+    apiKey: process.env.OUTRAY_API_KEY,
+  },
+  {
+    enabled: process.env.EXPOSE_TUNNEL === 'true',
+  }
+)
