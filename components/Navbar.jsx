76f0ff4c17da154fb1fc582bb0eed04728a14781
Adebayo <adedejiadebayo732@gmail.com>
2026-05-21 21:41:39 +0100

    diabled theme toggle on mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
 
         {/* Mobile Menu Icon (Placeholder) */}
         <div className="flex items-center gap-4 md:hidden">
-          <ThemeToggle />
+          {/* <ThemeToggle /> */}
           <button className="text-foreground" onClick={() => setOpen(!open)}>
               {open?<Close/>:
                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
